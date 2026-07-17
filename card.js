ec10364b3436b75f37d4338ac57f2b45630fe070
beabiiancheta <anchetabea1@gmail.com>
2026-07-17 08:38:38 +0800

    changeee

--- card.js
+++ card.js
@@ -1,29 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
+const cards = document.querySelectorAll(".certificate-card");
+const next = document.querySelector(".nav-btn.next");
+const prev = document.querySelector(".nav-btn.prev");
 
-    const cards = document.querySelectorAll(".certificate-card");
-    const next = document.querySelector(".nav-btn.next");
-    const prev = document.querySelector(".nav-btn.prev");
-    let current = 0;
+let current = 0;
 
-    function showCard(index) {
+function showCard(index){
 
-        cards.forEach(card => card.classList.remove("active"));
+    if(index >= cards.length) current = 0;
+    else if(index < 0) current = cards.length - 1;
+    else current = index;
 
-        if (index >= cards.length) current = 0;
-        if (index < 0) current = cards.length - 1;
+    cards.forEach(card => card.classList.remove("active"));
 
-        cards[current].classList.add("active");
-    }
+    cards[current].classList.add("active");
+}
 
-    next.addEventListener("click", () => {
-        current++;
-        showCard(current);
-    });
-
-    prev.addEventListener("click", () => {
-        current--;
-        showCard(current);
-    });
-
-    showCard(current);
+next.addEventListener("click", () => {
+    showCard(current + 1);
 });
+
+prev.addEventListener("click", () => {
+    showCard(current - 1);
+});
+
+showCard(0);
